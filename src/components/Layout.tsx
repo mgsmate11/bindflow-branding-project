85cafd05d3272ae00a5a460a6a12f7203b74345b
mgsmate11 <magos.mate@icloud.com>
2026-03-20 13:17:32 +0100

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,12 +2,14 @@ import { LangProvider } from '@/contexts/LangContext';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import FloatingContactButton from '@/components/FloatingContactButton';
+import ScrollToTop from '@/components/ScrollToTop';
 import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
     <LangProvider>
       <div className="min-h-screen bg-background flex flex-col">
+        <ScrollToTop />
         <Header />
         <main className="flex-1 pt-16">
           <Outlet />
